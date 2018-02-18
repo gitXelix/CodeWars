6d206639df9a32f26d2624c7255df896f18afef8
gitXelix <kacu196@gmail.com>
2018-02-18 15:15:31 +0100

    [6 Kyu] Find the missing letter - https://www.codewars.com/kata/5839edaa6754d6fec10000a2

--- src/findthemissingletter/Kata.java
+++ src/findthemissingletter/Kata.java
@@ -4,6 +4,7 @@ package findthemissingletter;
 https://www.codewars.com/kata/5839edaa6754d6fec10000a2
  */
 
+
 public class Kata
 {
     public static char findMissingLetter(char[] array)
--- src/findthemissingletter/SolutionTest.java
+++ src/findthemissingletter/SolutionTest.java
@@ -2,14 +2,13 @@ package findthemissingletter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class SolutionTest {
     public static void main(String[] args) {
         SolutionTest test = new SolutionTest();
         test.exampleTests();
     }
 
-    public void exampleTests() {
+    private void exampleTests() {
         assertEquals('e', Kata.findMissingLetter(new char[] { 'a','b','c','d','f' }));
         assertEquals('P', Kata.findMissingLetter(new char[] { 'O','Q','R','S' }));
     }
